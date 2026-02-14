58811e270f457af0775552cc742326d81ad1547b
Shoaib <shobekhan@yahoo.com>
2026-02-14 14:13:46 +0100

    Minor code update

--- cache/versioncache/app.js
+++ cache/versioncache/app.js
@@ -27,9 +27,13 @@ async function run() {
     await updateUser({ id: 1, name: "Alice Updated Again" });
     // DB WRITE
 
+    await getUser(1);
+    // CACHE MISS user:1:v3
+    // DB READ
+
     const final = await getUser(1);
-    console.log("Final user:", final);
     // CACHE HIT user:1:v3
+    console.log("Final user:", final);
 }
 
 run();
